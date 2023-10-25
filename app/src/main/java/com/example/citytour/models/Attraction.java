ab92a71e1c05db7ba68083d6ff1abc4341037ea4
Esta <esta.kempf@gmail.com>
2023-10-25 14:09:25 +0200

    Added longitude and altitude to as attraction attributes

--- app/src/main/java/com/example/citytour/models/Attraction.java
+++ app/src/main/java/com/example/citytour/models/Attraction.java
@@ -3,13 +3,15 @@ package com.example.citytour.models;
 public class Attraction {
     private String name;
     private int infoText;
-    private int imageResourceId;
+    private double latitude;
+    private double longitude;
 
     // Constructor
-    public Attraction(String name, int infoText, int imageResourceId) {
+    public Attraction(String name, int infoText, double latitude, double longitude) {
         this.name = name;
         this.infoText = infoText;
-        this.imageResourceId = imageResourceId;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     // Getters
@@ -21,29 +23,17 @@ public class Attraction {
         return infoText;
     }
 
-    public int getImageResourceId() {
-        return imageResourceId;
-    }
+    public double getLatitude() {return latitude;}
 
-    // Setters (if necessary)
-    public void setName(String name) {
-        this.name = name;
-    }
+    public double getLongitude(){return longitude;}
 
-    public void setInfoText(int infoText) {
-        this.infoText = infoText;
-    }
 
-    public void setImageResourceId(int imageResourceId) {
-        this.imageResourceId = imageResourceId;
-    }
 
     @Override
     public String toString() {
         return "Attraction{" +
                 "name='" + name + '\'' +
                 ", infoText='" + infoText + '\'' +
-                ", imageResourceId=" + imageResourceId +
                 '}';
     }
 }
--- app/src/main/java/com/example/citytour/models/AttractionHelper.java
+++ app/src/main/java/com/example/citytour/models/AttractionHelper.java
@@ -10,11 +10,11 @@ public class AttractionHelper {
     public static List<Attraction> getDefaultAttractions() {
         List<Attraction> attractions = new ArrayList<>();
 
-        attractions.add(new Attraction("Brandenburg Gate", R.string.info_brandenburgGate, R.string.info_brandenburgGate));
-        attractions.add(new Attraction("East Side Gallery", R.string.info_eastSideGallery, R.string.info_brandenburgGate));
-        attractions.add(new Attraction("TV Tower", R.string.info_tvTower, R.string.info_brandenburgGate));
-        attractions.add(new Attraction("Holocaust Memorial", R.string.info_holocaust, R.string.info_brandenburgGate));
-        attractions.add(new Attraction("Berlin Parliament", R.string.info_parliament, R.string.info_brandenburgGate));
+        attractions.add(new Attraction("Brandenburg Gate", R.string.info_brandenburgGate, 52.516181,13.376935));
+        attractions.add(new Attraction("East Side Gallery", R.string.info_eastSideGallery, 52.50444793701172,13.440881729125977));
+        attractions.add(new Attraction("TV Tower", R.string.info_tvTower, 52.52082824707031,13.409351348876953));
+        attractions.add(new Attraction("Holocaust Memorial", R.string.info_holocaust, 52.5138484,13.3787399));
+        attractions.add(new Attraction("Berlin Parliament", R.string.info_parliament, 52.5201123,13.3729545));
         // ... similarly add other attractions
 
         return attractions;
